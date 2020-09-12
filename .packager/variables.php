d0f766e46ea65ac6005dd8a1dbdfa2bf444fd998
Kerstens Maxim <maxim.kerstens@gmail.com>
2020-09-12 11:56:21 +0200

    add missing variable descriptions

--- .packager/variables.php
+++ .packager/variables.php
@@ -3,22 +3,22 @@
 return [
     'author_name' => [
         'title' => 'Author name',
-        'description' => null,
+        'description' => 'Who is the author?',
         'default' => config('packager.author_name')
     ],
     'author_email' => [
         'title' => 'Author E-mail',
-        'description' => null,
+        'description' => 'What is the author\'s e-mail?',
         'default' => config('packager.author_email')
     ],
     'author_homepage' => [
         'title' => 'Author Homepage',
-        'description' => null,
+        'description' => 'What is the author\'s website?',
         'default' => config('packager.author_homepage')
     ],
     'license' => [
         'title' => 'License',
-        'description' => null,
+        'description' => 'Under which license will it be released?',
         'default' => config('packager.license')
     ],
 ];
